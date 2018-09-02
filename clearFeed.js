e386f2d07bdd2c76c6f7b8c7109dee715e99bf47
OctoOsmo <tatarincev_al@bk.ru>
2018-09-03 00:08:16 +0300

    fix

--- clearFeed.js
+++ clearFeed.js
@@ -1,10 +1,10 @@
 function rem() {
     let el = document.getElementById('page_wall_posts');
     const count = el.childElementCount;
-    for (let i = 0; i < count - 20; i++) {
-        el.childNodes[0].remove();
+    for (let i = 1; i < count - 20; i++) {
+        el.childNodes[1].remove();
     }
-    return document.getElementById('page_wall_posts').childElementCount;
+    // return document.getElementById('page_wall_posts').childElementCount;
 }
 
 rem();
